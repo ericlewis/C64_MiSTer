cb8fa0d631cca698ad0b6d9c37665302ff28cff5
Eric Lewis <ericlewis777@gmail.com>
2026-04-05 23:45:43 -0400

    a

--- pocket/pocket_hpsio_compat.sv
+++ pocket/pocket_hpsio_compat.sv
@@ -63,14 +63,25 @@ data_loader #(
 );
 
 reg        dl_downloading_74a = 0;
-reg  [7:0] dl_slot_id_74a = SLOT_ROM;
-reg [31:0] dl_slot_size_74a = 0;
+wire [39:0] slot_info_sys;
+wire        slot_info_stb;
+reg  [7:0] current_slot_id = SLOT_ROM;
+reg [31:0] current_slot_size = 0;
+
+sync_fifo #(
+    .WIDTH(40)
+) slot_info_fifo (
+    .clk_write (clk_74a),
+    .clk_read  (clk_sys),
+    .write_en  (dataslot_requestwrite),
+    .data      ({dataslot_requestwrite_id[7:0], dataslot_requestwrite_size}),
+    .data_s    (slot_info_sys),
+    .write_en_s(slot_info_stb)
+);
 
 always @(posedge clk_74a) begin
     if (dataslot_requestwrite) begin
         dl_downloading_74a <= 1;
-        dl_slot_id_74a <= dataslot_requestwrite_id[7:0];
-        dl_slot_size_74a <= dataslot_requestwrite_size;
         dl_start_74a <= ~dl_start_74a;
     end
     else if (dataslot_allcomplete) begin
@@ -79,15 +90,12 @@ always @(posedge clk_74a) begin
 end
 
 reg        dl_s0 = 0, dl_s1 = 0;
-reg  [7:0] dl_slot_id_s0 = SLOT_ROM, dl_slot_id_s1 = SLOT_ROM;
-reg [31:0] dl_slot_size_s0 = 0, dl_slot_size_s1 = 0;
 reg        dl_stream_prev = 0;
 reg        ioctl_download_prev = 0;
 reg  [7:0] dl_tail_hold = 0;
 reg        dl_start_74a = 0, dl_start_s0 = 0, dl_start_s1 = 0, dl_start_prev = 0;
 wire       active_now = dl_s1 || dl_wr || (dl_tail_hold != 0);
-wire [7:0] active_ioctl_index = dl_wr ? slot_to_ioctl_index({4'd0, dl_addr[27:24]})
-                                      : slot_to_ioctl_index(dl_slot_id_s1);
+wire [7:0] active_ioctl_index = slot_to_ioctl_index(current_slot_id);
 
 always @(posedge clk_sys) begin
     load_start <= 0;
@@ -95,16 +103,17 @@ always @(posedge clk_sys) begin
 
     dl_s0 <= dl_downloading_74a;
     dl_s1 <= dl_s0;
-    dl_slot_id_s0 <= dl_slot_id_74a;
-    dl_slot_id_s1 <= dl_slot_id_s0;
-    dl_slot_size_s0 <= dl_slot_size_74a;
-    dl_slot_size_s1 <= dl_slot_size_s0;
-    slot_size <= dl_slot_size_s1;
     dl_start_s0 <= dl_start_74a;
     dl_start_s1 <= dl_start_s0;
     dl_start_prev <= dl_start_s1;
     dl_stream_prev <= dl_s1;
     ioctl_download_prev <= ioctl_download;
+
+    if (slot_info_stb) begin
+        current_slot_id <= slot_info_sys[39:32];
+        current_slot_size <= slot_info_sys[31:0];
+        slot_size <= slot_info_sys[31:0];
+    end
 
     if (dl_s1) dl_tail_hold <= 8'd96;
     else if (dl_stream_prev) dl_tail_hold <= 8'd96;
